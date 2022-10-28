c2f8305c5756fe991f2d077e84876f112ca4cb8d
ndalfonsin <ndalfonsin@gmail.com>
2022-10-28 19:48:48 -0300

    upload

--- 02-algoritmos/ejercicios_basicos_p2.py
+++ 02-algoritmos/ejercicios_basicos_p2.py
@@ -20,3 +20,24 @@ for cabecera in range(1,11):
         print(f"{cabecera} X {contador} = {mult}")
         contador += 1
 
+
+
+"""
+Ejercicio 7 
+hacer un programa que muestre todos los numeros impares entre 2 numeros que elija el usuario
+"""
+
+numeroU1 = int(input("Inserte el primer numero: "))
+numeroU2 = int(input("inserte el segundo numero: "))
+
+if numeroU1 > numeroU2:
+    for contador in range((numeroU2 + 1),(numeroU1)):
+        if contador % 2 !=0:
+            print(contador)
+elif numeroU1 < numeroU2:
+    for  contador in range((numeroU1 + 1), (numeroU2)):
+        if contador % 2 !=0:
+            print(contador)
+else:
+    print(f"{numeroU1} = {numeroU2}")
+
