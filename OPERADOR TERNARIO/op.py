39cc1e63f79c3dfd491b05ce1ad4c5fd38b05ad7
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-06-11 02:20:23 -0300

    upload

--- OPERADOR TERNARIO/op.py
+++ OPERADOR TERNARIO/op.py
@@ -5,3 +5,9 @@ print((b,a)[a<b])
 print(f"({s[:3-len(s)]}) {s[3:6-len(s)]}-{s[6:]}")
 
 #1234567890   (123) 456-7890
+
+
+            #arranca, corte, suma 2a irac
+for i in range(1, n+1, 2):
+   print(i)
+   print('hello')
